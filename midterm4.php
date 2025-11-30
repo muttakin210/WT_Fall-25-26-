afdcf5ff4eeb40abbfd9713a8d14b18f30af79a2
muttakin210 <zihanm168@gmail.com>
2025-12-01 00:42:03 +0600

    jubayer

---
+++ midterm4.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>my first task</title>
+</head>
+<body>
+    <fieldset>
+    <h1>result</h1>
+    <p><b>i</b> am <u><strong><i>md.muttakin</i></strong></u> im study at aiub.Text formatting tags in HTML are used to style or emphasize text in various ways, such as 
+making it bold,<em> italic</em>, <small>underlined</small>, or <mark>marked</mark>. They help enhance readability and meaning. </p><br>
+ <a href="https://portal.aiub.edu/Student/Home/Index/5" target="_blank">AIUB</a><br><br>
+ <img src "https://share.google/images/dRf7VLgRDuZB6TH9g" alt="an image from google" width="300"
+ height="300"><br>
+
+ <ol>
+    <li>atik</li>
+    <li>miraj</li>
+</ol>
+<ul>
+    <li>atik</li>
+    <li>miraj</li>
+</ul>
+<table border="1">
+  <tr>
+    <th colspan="3">personal info</th>
+</tr>
+    <tr>
+        <th>name</th>
+        <th>age</th>
+        <th>city</th>
+</tr>
+<tr>
+    <td>munshi</td>
+        <td>21</td>
+        <td>dhaka</td>
+        </tr>
+</teble>
+
+</fieldset>
+<br><br><br>
+
+<fieldset>
+<from>
+   <label>name:</label><br>
+    <input type="text"><br><br>
+     <label>email:</label><br>
+    <input type="text"><br><br>
+    <label>password:</label><br>
+    <input type="text"><br><br>
+
+    <select>
+        <option>usa</option>
+        <option>canada</option>
+        <option>garman</option>
+        <option>finland</option>
+</select><br><br><br>
+
+   <label>hobbies:</label>
+   <input type="checkbox" value="reading" > reading
+<input type="checkbox" value="traveling" > traveling
+<input type="checkbox" value="cooking" > cooking
+<br><br>
+<label>uploaded file</label>
+<input type="file">
+<br><br><br>
+    <input type="submit">
+</fieldset>
+</from>
+</body>
+</html>
