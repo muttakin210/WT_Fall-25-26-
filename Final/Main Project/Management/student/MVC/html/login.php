d865572d4fe866f553094b0299509163bcb117e7
muttakin210 <zihanm168@gmail.com>
2026-01-17 12:42:34 +0600

    login.phpv3

--- Final/Main Project/Management/student/MVC/html/login.php
+++ Final/Main Project/Management/student/MVC/html/login.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html>
 <head>
-    
     <title>Login | AIUB Notes</title>
     <link rel="stylesheet" href="../css/login.css">
 </head>
@@ -11,11 +10,21 @@
         <h2>Login</h2>
         <p>AIUB Student Notes & Slides</p>
 
-        <form>
-            <input type="text" placeholder="Student ID" required>
-            <input type="password" placeholder="Password" required>
+        <!-- show messages -->
+        <?php
+        if (isset($_GET['error'])) {
+            echo "<p style='color:red; text-align:center;'>Invalid email or password</p>";
+        }
+        if (isset($_GET['success'])) {
+            echo "<p style='color:green; text-align:center;'>Registration successful! Please login.</p>";
+        }
+        ?>
 
-            <button type="submit">Login</button>
+        <form method="POST" action="../php/login.php">
+            <input type="email" name="email" placeholder="Email Address" required>
+            <input type="password" name="password" placeholder="Password" required>
+
+            <button type="submit" name="login">Login</button>
         </form>
 
         <p class="link">
