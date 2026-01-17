03ce0cb4dadf1a60b30bdf57e5f45ca3cbc06861
muttakin210 <zihanm168@gmail.com>
2026-01-17 12:41:29 +0600

    login v2

--- Final/Main Project/Management/student/MVC/php/login.php
+++ Final/Main Project/Management/student/MVC/php/login.php
@@ -7,26 +7,27 @@ if (isset($_POST['login'])) {
     $email = mysqli_real_escape_string($conn, $_POST['email']);
     $password = $_POST['password'];
 
-    // Check if email exists
     $sql = "SELECT * FROM students WHERE email='$email'";
     $result = mysqli_query($conn, $sql);
 
-    if (mysqli_num_rows($result) === 1) {
+    if (mysqli_num_rows($result) == 1) {
+
         $user = mysqli_fetch_assoc($result);
 
-        
         if (password_verify($password, $user['password'])) {
-    
             $_SESSION['user_id'] = $user['id'];
             $_SESSION['full_name'] = $user['full_name'];
 
             header("Location: ../html/dashboard.php");
             exit();
         } else {
-            $error = "Incorrect password!";
+            header("Location: ../html/login.php?error=1");
+            exit();
         }
+
     } else {
-        $error = "Email not registered!";
+        header("Location: ../html/login.php?error=1");
+        exit();
     }
 }
 ?>
