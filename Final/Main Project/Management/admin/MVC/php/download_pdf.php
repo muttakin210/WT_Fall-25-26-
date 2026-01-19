84c242c76f13d572d1dca9436a3c9b2b1b1ee28b
muttakin210 <zihanm168@gmail.com>
2026-01-19 11:39:22 +0600

    v2

--- Final/Main Project/Management/admin/MVC/php/download_pdf.php
+++ Final/Main Project/Management/admin/MVC/php/download_pdf.php
@@ -12,4 +12,17 @@ if (!isset($_GET['id'])) {
     exit();
 }
 
+$note_id = mysqli_real_escape_string($conn, $_GET['id']);
+$admin_id = $_SESSION['admin_id'];
+
+// Check if admin_id or teacher_id column exists and fetch accordingly
+$check_admin_column = "SHOW COLUMNS FROM materials LIKE 'admin_id'";
+$admin_result = mysqli_query($conn, $check_admin_column);
+
+if (mysqli_num_rows($admin_result) > 0) {
+    $sql = "SELECT * FROM materials WHERE id = '$note_id' AND admin_id = '$admin_id'";
+} else {
+    $sql = "SELECT * FROM materials WHERE id = '$note_id' AND teacher_id = '$admin_id'";
+}
+
 ?>
