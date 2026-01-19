06348bd3c52c901013ffa52f476e427bff401600
muttakin210 <zihanm168@gmail.com>
2026-01-19 11:03:55 +0600

    v4

--- Final/Main Project/Management/admin/MVC/html/Dashboard.php
+++ Final/Main Project/Management/admin/MVC/html/Dashboard.php
@@ -1,29 +1,29 @@
 <?php
 session_start();
-if (!isset($_SESSION['user_type']) || $_SESSION['user_type'] != 'teacher') {
-    header("Location: ../../../Teacher/MVC/html/teacher_login.php");
+if (!isset($_SESSION['user_type']) || $_SESSION['user_type'] != 'admin') {
+    header("Location: ../../../student/MVC/html/login.php");
     exit();
 }
 
-$teacher_id = $_SESSION['teacher_id']; 
-$teacher_name = $_SESSION['full_name'];
+$admin_id = $_SESSION['admin_id']; 
+$admin_name = $_SESSION['full_name'];
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Teacher Dashboard | AIUB Notes</title>
+    <title>Admin Dashboard | AIUB Notes</title>
     <link rel="stylesheet" href="../css/dashboard.css">
 </head>
 <body>
     <div class="navbar">
-        <h2>AIUB Notes – Teacher</h2>
+        <h2>AIUB Notes – Admin</h2>
         <a href="../php/logout.php">Logout</a>
     </div>
 
     <div class="container">
-        <h1>Welcome, MR. <?php echo htmlspecialchars($teacher_name); ?></h1>
-        <p>Manage your courses and materials</p>
+        <h1>Welcome, <?php echo htmlspecialchars($admin_name); ?></h1>
+        <p>Manage courses and materials</p>
 
         <div class="cards">
             <div class="card">
