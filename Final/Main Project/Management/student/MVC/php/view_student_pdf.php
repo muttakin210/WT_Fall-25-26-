9a8dc0268a44ddc6a44541ec5f01d13f47083c7e
muttakin210 <zihanm168@gmail.com>
2026-01-19 11:47:30 +0600

    v2

--- Final/Main Project/Management/student/MVC/php/view_student_pdf.php
+++ Final/Main Project/Management/student/MVC/php/view_student_pdf.php
@@ -18,16 +18,4 @@ $note_id = mysqli_real_escape_string($conn, $_GET['id']);
 $sql = "SELECT * FROM materials WHERE id = '$note_id'";
 $result = mysqli_query($conn, $sql);
 
-if (mysqli_num_rows($result) == 1) {
-    $row = mysqli_fetch_assoc($result);
-    
-    header('Content-Type: application/pdf');
-    header('Content-Disposition: inline; filename="' . $row['file_name'] . '"');
-    header('Content-Length: ' . strlen($row['file_data']));
-    
-    echo $row['file_data'];
-} else {
-    echo "<script>alert('File not found');</script>";
-    echo "<script>window.location.href='../html/search_notes.php';</script>";
-}
 ?>
