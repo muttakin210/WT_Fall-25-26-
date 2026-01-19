9ce228ebfdeacba125a0fe0df4f2e49f4b3665c3
muttakin210 <zihanm168@gmail.com>
2026-01-19 11:46:09 +0600

    v2

--- Final/Main Project/Management/student/MVC/php/download_student_pdf.php
+++ Final/Main Project/Management/student/MVC/php/download_student_pdf.php
@@ -14,4 +14,17 @@ if (!isset($_GET['id'])) {
 
 $note_id = mysqli_real_escape_string($conn, $_GET['id']);
 
+// Students can download any note - no restriction by admin_id
+$sql = "SELECT * FROM materials WHERE id = '$note_id'";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) == 1) {
+    $row = mysqli_fetch_assoc($result);
+    
+    header('Content-Type: application/pdf');
+    
+} else {
+    echo "<script>alert('File not found');</script>";
+    echo "<script>window.location.href='../html/search_notes.php';</script>";
+}
 ?>
